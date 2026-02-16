e8d4de8be54295978cd54159535ffc919377d635
ljw097 <njbsandrewlee@gmail.com>
2026-02-16 16:36:36 +0000

    Feat: added Find friend system.

--- client.js
+++ client.js
@@ -1,4 +1,4 @@
-const {signup, connect, login, join_room, send_message} = require('./fe/module');
+const {signup, connect, login, join_room, send_message, get_friend} = require('./fe/module');
 
 
 
@@ -7,11 +7,18 @@ const {signup, connect, login, join_room, send_message} = require('./fe/module')
 const uid = 'test';
 const upw = 'testtest123!@';
 const upw_c = 'testtest123!@';
-const email = 'test@test.com';
+const email = 'njbsandrewlee@gmail.com';
 const phone = '01072684290';
-
+/*
 (async () => {
     const loggggg = await login(uid, upw);
     console.log(loggggg.token)
     connect(loggggg.token);
+})();
+*/
+
+(async () => {
+    const resp = await get_friend('uid', uid);
+    console.log(resp);
+    console.log(resp.id);
 })();
--- fe/module.js
+++ fe/module.js
@@ -3,6 +3,7 @@ const axios = require('axios');
 const socket = io('http://localhost:3000', {
     autoConnect: false
 });
+const serverUrl = "http://localhost:3000"
 
 
 async function connect(token) {
@@ -11,8 +12,9 @@ async function connect(token) {
 };
 
 async function signup(uid, upw, upw_c, email, phone) {
+    const signupUrl = serverUrl + '/auth/signup'
     try {
-        const res = await axios.post('http://localhost:3000/auth/signup', {
+        const res = await axios.post(signupUrl, {
             uid: `${uid}`,
             upw: `${upw}`,
             upw_c: `${upw_c}`,
@@ -27,8 +29,10 @@ async function signup(uid, upw, upw_c, email, phone) {
 };
 
 async function login(uid, upw) {
+    const loginUrl = serverUrl + '/auth/login';
+    //console.log(loginUrl)
     try {
-        const res = await axios.post('http://localhost:3000/auth/login', {
+        const res = await axios.post(loginUrl, {
             uid: `${uid}`,
             upw: `${upw}`
         });
@@ -58,7 +62,6 @@ async function join_room(from, to) {
     return new Promise((resolve, reject) => {
         socket.emit('join_room', { from, to }, (res) => {
             if (res.ok) {
-                openRoomUi();
                 resolve(res.roomId); // 여기서 실제 roomId 반환
             } else {
                 console.error('join_room: ', res.error);
@@ -66,12 +69,11 @@ async function join_room(from, to) {
             }
         });
     });
-}
+};
 
 async function send_message(msg) {
     socket.emit('send_message', msg, (res) => {
         if (res.ok) {
-            addMessageToUi({ ...msg, status:'sent' });
             console.log('send_message: ', msg);
         } else {
             addMessageToUi({ ...msg, status:'failed'});
@@ -79,5 +81,23 @@ async function send_message(msg) {
     });
 };
 
+async function get_friend(type, input) {
+    const getFriendUrl = serverUrl + '/auth/friend';
+    //console.log(getFriendUrl);
+    try {
+        const res = await axios.post(getFriendUrl, {
+            type: `${type}`,
+            data: `${input}`
+        });
+        if (res.data.ok) {
+            return res.data;
+        } else {
+            return { ok: false };
+        };
+    } catch (err) {
+        return err.response.data;
+    };
+}
 
-module.exports = {signup, connect, login, join_room, send_message};
+
+module.exports = {signup, connect, login, join_room, send_message, get_friend};
